118cc6bf7b4bb38208aa9f710f94d75cbc5ee013
JavierVinalCosta <148990968+JavierVinalCosta@users.noreply.github.com>
2023-11-07 12:04:11 +0100

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,53 +1,60 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+# __Author__="Javier Vinal Costa"
 
-import random
+import  random
 
-# Función que determina si un numero es par.
+# Función que determina si un número es par.
+def esPar(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
 
-def esPar(numero) :
-    if (numero%2==0) :
-        return True 
-   
+# Función que determina si un número es impar.
+def esImpar(numero):
+    if numero % 2 != 0:
+        return True
+    else:
+        return False
 
-def esImpar(numero) :
-    if(numero%2!=0):
-        
-        return False 
+def generarPares(valores, inicio):
+    pares = []
+    numero = inicio
 
-def generarPares(valores, inicio) :
-    pares=[]
-    numero=inicio
-    if esImpar(inicio) :
-        numero=inicio+1
+    if esImpar(inicio):
+        numero = inicio + 1
+
     for i in range(valores):
         pares.append(numero)
         numero += 2
+
     return pares
 
+def generarImpares(valores, inicio):
+    impares = []
+    numero = inicio
 
-def generarImpares(valores, inicio) :
-    impares=[]
-    numero=inicio
-    if esPar(inicio) :
-        numero=inicio+1
+    if esPar(inicio):
+        numero = inicio + 1
+
     for i in range(valores):
         impares.append(numero)
-        numero+=2
-    return impares
+        numero += 2
 
+    return impares
 
 # Programa principal
 def main():
     print("Par e impar")
-    n=int(input("Introduzca un numero: "))
-    print("{0} es par --> {1}.".format(n,esPar(n)))
-    m=int(input("Introduzca el número de valores: "))
-    i=int(input("Introduzca el número inicial: "))
-    x=generarPares(m,i)
-    y=generarImpares(m,i)
-    print("Impares: ",y)
-    print("Pares: ",x)    
+    n = int(input("Introduzca un número: "))
+    print("{0} es par --> {1}.".format(n, esPar(n)))
 
-if __name__== "__main__" :
-   main()
+    m = int(input("Introduzca el número de valores: "))
+    i = int(input("Introduzca el número inicial: "))
+    x = generarPares(m, i)
+    y = generarImpares(m, i)
+    print("Impares:", y)
+    print("Pares:", x)
+
+if __name__ == "__main__":
+    main()
